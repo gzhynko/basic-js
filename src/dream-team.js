7d5072731eba7d0ff80cd6654f20f5296da4d9e5
GZhynko <gleb.zhinko@gmail.com>
2020-09-12 17:43:29 +0300

    feat: do dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let teamName = [];
+
+  if(!Array.isArray(members)) return false;
+  
+  for(var i = 0; i <= members.length; i++){
+    if(typeof members[i] === 'string'){
+      teamName.push(members[i].trim().substr(0, 1).toUpperCase());
+    }
+  }
+
+  return teamName.sort((f, s) => f.charCodeAt(0) - s.charCodeAt(0)).join("");
 };
