705a229f56b7a65880a704889a1cb5aa26a78ac8
Gleb Zhynko <gleb.zhinko@gmail.com>
2020-10-18 12:03:18 +0300

    refactor: remove fix for double floating point

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,9 +4,6 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  // fix for a double floating point in one of the tests
-  if(sampleActivity == '11.3231.3213124') sampleActivity = '11.32313213124';
-
   if(sampleActivity !== '' + parseFloat(sampleActivity) || sampleActivity <= 0 || sampleActivity > 15) return false;
 
   let rateConstant = -.693 / HALF_LIFE_PERIOD;
